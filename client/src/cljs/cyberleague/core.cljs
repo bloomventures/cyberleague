1deaae0548e6919472eb2fb0b9115ce3f79a1c88
Rafal Dittwald <rafal.dittwald@gmail.com>
2014-09-26 20:57:39 -0400

    card view placeholders

--- client/src/cljs/cyberleague/core.cljs
+++ client/src/cljs/cyberleague/core.cljs
@@ -3,29 +3,106 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [cljs.core.async :refer [put! chan <! sub pub]]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [goog.events :as events]
+            [cljs.reader :as reader])
+  (:import [goog.net XhrIo EventType]))
 
 (enable-console-print!)
 
-(def app-state (atom {}))
+(defn edn-xhr
+  "Send an xhr request with the given data as EDN
+  Implementation taken from om-sync."
+  [{:keys [method url data on-complete on-error auth]}]
+  (let [xhr (XhrIo.)]
+    (when on-complete
+      (events/listen xhr EventType.SUCCESS
+        (fn [e] (on-complete (reader/read-string (.getResponseText xhr))))))
+    (when on-error
+      (events/listen xhr EventType.ERROR
+        (fn [e] (on-error {:error (.getResponseText xhr)}))))
+    (.send xhr url (.toUpperCase (name method)) (when data (pr-str data))
+           #js {"Content-Type" "application/edn"
+                "Accept" "application/edn"
+                "Authorization" (str "Basic " (js/btoa (string/join ":" auth)))})))
 
+(def app-state (atom {:cards [{:type :games}
+                              {:type :game
+                               :id 123}
+                              {:type :rules
+                               :id 123}
+                              {:type :bot
+                               :id 345}
+                              {:type :code
+                               :id 345}
+                              {:type :match
+                               :id 456}]}))
 
-(defn games-card-view [data owner])
+(defn games-card-view [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:className "card"}
+        (apply dom/div nil
+          (map (fn [game] (dom/a nil (game :name) (game :bot-count))) (data :games)))))))
 
-(defn game-card-view [data owner])
+(defn game-card-view [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:className "card"} "GAME")
+      )))
 
-(defn rules-card-view [data owner])
+(defn rules-card-view [data owner]
+  (reify
+    om/IRender
+    (render [_]
 
-(defn code-card-view [data owner])
+      (dom/div #js {:className "card"} "RULES")
+      )))
 
-(defn match-card-view [data owner])
+(defn bot-card-view [data owner]
+  (reify
+    om/IRender
+    (render [_]
+
+      (dom/div #js {:className "card"} "BOT")
+      )))
+
+(defn code-card-view [data owner]
+  (reify
+    om/IRender
+    (render [_]
+
+      (dom/div #js {:className "card"} "CODE")
+      )))
+
+(defn match-card-view [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:className "card"} "MATCH")
+      )))
 
 (defn app-view [data owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div nil "Hello World!"))))
+      (apply dom/div #js {:className "cards"}
+        (map (fn [card]
+               (om/build (condp = (:type card)
+                           :game game-card-view
+                           :games games-card-view
+                           :rules rules-card-view
+                           :bot bot-card-view
+                           :code code-card-view
+                           :match match-card-view) data)) (data :cards))))))
 
-(om/root app-view app-state {:target (. js/document (getElementById "app"))})
+(defn init []
+  (om/root app-view app-state {:target (. js/document (getElementById "app"))})
 
-(defn init [])
+  (edn-xhr {:url "/api/games"
+            :method :get
+            :on-complete (fn [data]
+                           (swap! app-state assoc :games (:games data)))})
+  )
