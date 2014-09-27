39d6f2fc2b9b0e37a4c14133ee000106160d588c
James N. V. Cash <james.nvc@gmail.com>
2014-09-27 15:33:35 -0400

    switching to string keys for game state, to avoid annoying clojurescript issue

--- pog/src/cljs/pog/games.cljs
+++ pog/src/cljs/pog/games.cljs
@@ -16,25 +16,25 @@
 ;; Goofspiel
 (comment
   ; Example game state
-  {:player-cards
+  {"player-cards"
    {12345 #{ 1 2 3 13 }
     54321 #{ 1 2 3 13 }}
-   :trophy-cards #{ 1 2 3 13 }
-   :current-trophy 4
-   :history  [ { 12345 1 54321 1 :trophy 1 } … ] }
+   "trophy-cards" #{ 1 2 3 13 }
+   "current-trophy" 4
+   "history"  [ { 12345 1 54321 1 "trophy" 1 } … ] }
   ; Example move
   12
   )
 
 (defn set-next-trophy
   "Helper function for setting the next tropyh"
-  [{:keys [trophy-cards] :as state}]
+  [{:strs [trophy-cards] :as state}]
   (if (empty? trophy-cards)
-    (dissoc state :current-trophy)
+    (dissoc state "current-trophy")
     (let [next-trophy (rand-nth (vec trophy-cards))]
       (-> state
-          (update-in [:trophy-cards] disj next-trophy)
-          (assoc :current-trophy next-trophy)))))
+          (update-in ["trophy-cards"] disj next-trophy)
+          (assoc "current-trophy" next-trophy)))))
 
 (defmethod make-engine "goofspiel"
   [_]
@@ -48,34 +48,34 @@
 
     (init-state [_ players]
       (set-next-trophy
-        {:player-cards (reduce (fn [a pl-id] (assoc a pl-id (set (range 1 14))))
+        {"player-cards" (reduce (fn [a pl-id] (assoc a pl-id (set (range 1 14))))
                                {} players)
-         :trophy-cards (set (range 1 14))
-         :current-trophy nil
-         :history [ ]}))
+         "trophy-cards" (set (range 1 14))
+         "current-trophy" nil
+         "history" [ ]}))
 
     (legal-move? [_ state player move]
-      (contains? (get-in state [:player-cards player]) move))
+      (contains? (get-in state ["player-cards" player]) move))
 
-    (next-state [_ {:keys [player-cards trophy-cards current-trophy history] :as state} moves]
-      (-> (reduce-kv (fn [st player move] (update-in st [:player-cards player] disj move))
+    (next-state [_ {:strs [player-cards trophy-cards current-trophy history] :as state} moves]
+      (-> (reduce-kv (fn [st player move] (update-in st ["player-cards" player] disj move))
                      state moves)
-          (update-in [:history] conj (merge {:trophy current-trophy} moves))
+          (update-in ["history"] conj (merge {"trophy" current-trophy} moves))
           set-next-trophy))
 
     (game-over? [_ state]
-      (and (empty? (:trophy-cards state)) (nil? (:current-trophy state))))
+      (and (empty? (state "trophy-cards")) (nil? (state "current-trophy"))))
 
     (winner [_ state]
       (let [scores (reduce (fn [sc mv]
-                             (let [trophy (:trophy mv)
-                                   mv (dissoc mv :trophy)
+                             (let [trophy (mv "trophy")
+                                   mv (dissoc mv "trophy")
                                    highest (reduce max 0 (vals mv))
                                    winner (filter (comp (partial = highest) second) mv)]
                                (if (= 1 (count winner))
                                  (update-in sc [(ffirst winner)] (fnil (partial + trophy) 0))
                                  sc)))
-                           {} (:history state))
+                           {} (state "history"))
             high-score (reduce max 0 (vals scores))
             winner (filter (comp (partial = high-score) second) scores)]
         (when (= 1 (count winner))
