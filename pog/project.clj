9929b4051644449c56a9d358344dd711ab1ff724
James N. V. Cash <james.nvc@gmail.com>
2014-09-27 11:36:43 -0400

    start work on precompile

--- pog/project.clj
+++ pog/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.datomic/datomic-pro "0.9.4899"]
+                 [me.raynes/fs "1.4.4"]
                  [org.clojure/clojurescript "0.0-2356"]]
 
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
