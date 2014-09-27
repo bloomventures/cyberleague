fecfed7749933c4c8b2fe2ae5cac75b37cdbf6a5
James N. V. Cash <james.nvc@gmail.com>
2014-09-27 11:26:09 -0400

    basic goofspiel game engine seems complete

--- pog/test/cljs/pog/games_test.cljs
+++ pog/test/cljs/pog/games_test.cljs
@@ -20,6 +20,7 @@
         (is (game/legal-move? g state 12345 3))
         (is (not (game/legal-move? g state 12345 13)))
         (is (not (game/legal-move? g state 12345 6))))))
+
   (testing "progressing game state"
     (let [g (game/make-engine {:game/name "goofspiel"})
           state (game/init-state g 12345 54321)]
@@ -31,10 +32,27 @@
 
       (testing "progressing the state with moves"
         (let [trophy (:current-trophy state)
-              new-state (game/next-state g state [{12345 13 54321 10}])]
-          (println new-state)
+              new-state (game/next-state g state {12345 13 54321 10})]
           (is (not= (:current-trophy new-state) trophy))
           (is (= 1 (count (:history new-state))))
           (is (= {12345 13 54321 10 :trophy trophy}
                  (first (:history new-state))))
-          )))))
+          (is (not (contains? (get-in new-state [:player-cards 12345]) 13)))
+          (is (not (contains? (get-in new-state [:player-cards 54321]) 10)))
+          (is (not (game/game-over? g new-state)))
+          (let [third-state (game/next-state g new-state {12345 12 54321 9})]
+            (is (= 2 (count (:history third-state))))
+            (is (not (game/game-over? g third-state))))))))
+
+  (testing "Finishing game"
+    (let [g (game/make-engine {:game/name "goofspiel"})
+          almost-done-state {:player-cards {12345 #{1}
+                                            54321 #{13}}
+                             :trophy-cards #{}
+                             :current-trophy 1
+                             :history (vec (for [i (range 13 1 -1)]
+                                             {12345 i 54321 (dec i) :trophy i}))}]
+      (is (not (game/game-over? g almost-done-state)))
+      (let [done-state (game/next-state g almost-done-state {12345 1 54321 13})]
+        (is (game/game-over? g done-state))
+        ))))
