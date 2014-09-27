5a8a24171b620c98992c1db905594d9aeea9d608
James N. V. Cash <james.nvc@gmail.com>
2014-09-27 11:26:09 -0400

    basic goofspiel game engine seems complete

--- pog/src/cljs/pog/games.cljs
+++ pog/src/cljs/pog/games.cljs
@@ -6,7 +6,7 @@
   (init-state [_ player-1 player-2] "Create the initial state of the game")
   (legal-move? [_ state player move] "Is the player's move for the given game state legal?")
   (next-state [_ state moves] "Calculate the next state, given the current state
-                              and an array of player moves like [{:player p1 :move m}]")
+                              and an map of player moves like {player-1-id move1 player-2-id move2}")
   (game-over? [_ state] "Is the game in a finished state?")
   (winner [_ state] "If the game is over, return the winner of the game")
 
@@ -30,10 +30,12 @@
 (defn set-next-trophy
   "Helper function for setting the next tropyh"
   [{:keys [trophy-cards] :as state}]
-  (let [next-trophy (rand-nth (vec trophy-cards))]
-    (-> state
-        (update-in [:trophy-cards] disj next-trophy)
-        (assoc :current-trophy next-trophy))))
+  (if (empty? trophy-cards)
+    (dissoc state :current-trophy)
+    (let [next-trophy (rand-nth (vec trophy-cards))]
+      (-> state
+          (update-in [:trophy-cards] disj next-trophy)
+          (assoc :current-trophy next-trophy)))))
 
 (defmethod make-engine "goofspiel"
   [_]
@@ -56,12 +58,25 @@
       (contains? (get-in state [:player-cards player]) move))
 
     (next-state [_ {:keys [player-cards trophy-cards current-trophy history] :as state} moves]
-      (let [next-trophy (rand-nth (vec trophy-cards))]
-        (-> state
-            (update-in [:history] conj (apply merge {:trophy current-trophy} moves))
-            set-next-trophy)))
+      (-> (reduce-kv (fn [st player move] (update-in st [:player-cards player] disj move))
+                     state moves)
+          (update-in [:history] conj (merge {:trophy current-trophy} moves))
+          set-next-trophy))
 
-    (game-over? [_ state])
+    (game-over? [_ state]
+      (and (empty? (:trophy-cards state)) (nil? (:current-trophy state))))
 
     (winner [_ state]
-      )))
+      (let [scores (reduce (fn [sc mv]
+                             (let [trophy (:trophy mv)
+                                   mv (dissoc mv :trophy)
+                                   highest (reduce max 0 (vals mv))
+                                   winner (filter (comp (partial = highest) second) mv)]
+                               (if (= 1 (count winner))
+                                 (update-in sc [(ffirst winner)] (fnil (partial + trophy) 0))
+                                 sc)))
+                           {} (:moves state))
+            high-score (reduce max 0 (vals scores))
+            winner (filter (comp (partial = high-score) second) scores)]
+        (when (= 1 (count winner))
+          (ffirst winner))))))
