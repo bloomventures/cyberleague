e347c83d86b92f9e5039584d379425a98695bdeb
Rafal Dittwald <rafal.dittwald@gmail.com>
2014-09-27 20:03:57 -0400

    update update-ranking to use glicko

--- pog/project.clj
+++ pog/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.datomic/datomic-pro "0.9.4899"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  [me.raynes/fs "1.4.4"]
                  [org.clojure/clojurescript "0.0-2356"]]
 
