85599482b115699056b814566b9c0c912e9ca85c
James N. V. Cash <james.nvc@gmail.com>
2014-11-23 14:08:07 -0500

    fix bug in resolving tied ultimate tic-tac-toe games

--- test/cljs/pog/games_test.cljs
+++ test/cljs/pog/games_test.cljs
@@ -178,4 +178,80 @@
         (is (not (game/game-over? g almost-done-state)))
         (let [done-state (game/next-state g almost-done-state {12345 [2 2]})]
           (is (game/game-over? g done-state))
-          (is (= 12345 (game/winner g done-state))))))))
+          (is (= 12345 (game/winner g done-state))))))
+
+    (testing "with a drawn board"
+      (let [g (game/make-engine {:game/name "ultimate tic-tac-toe"})
+            tied-state {"grid"
+                        [["o" nil "o" nil "o" "x" "o" "x" nil]
+                         [nil "x" "o" nil "o" nil "o" nil nil]
+                         ["x" "x" "x" "o" nil "o" "x" nil nil]
+                         ["x" "x" "x" "x" "o" "o" "o" "o" "x"]
+                         ["o" "x" "x" "x" "o" "o" "o" "x" "x"]
+                         [nil "x" nil nil "x" nil "o" "x" nil]
+                         ["x" "o" "x" "o" nil "o" "x" "x" "x"]
+                         ["o" "o" "x" "o" "x" "x" nil "o" "x"]
+                         [nil nil "o" "o" "o" nil "o" nil nil]],
+                        "history"
+                        [{"player" 56789, "move" [2 2]}
+                         {"player" 98765, "move" [2 5]}
+                         {"player" 56789, "move" [5 7]}
+                         {"player" 98765, "move" [7 0]}
+                         {"player" 56789, "move" [0 7]}
+                         {"player" 98765, "move" [7 7]}
+                         {"player" 56789, "move" [7 8]}
+                         {"player" 98765, "move" [8 2]}
+                         {"player" 56789, "move" [2 1]}
+                         {"player" 98765, "move" [1 2]}
+                         {"player" 56789, "move" [2 6]}
+                         {"player" 98765, "move" [6 5]}
+                         {"player" 56789, "move" [5 4]}
+                         {"player" 98765, "move" [4 4]}
+                         {"player" 56789, "move" [4 1]}
+                         {"player" 98765, "move" [1 6]}
+                         {"player" 56789, "move" [6 6]}
+                         {"player" 98765, "move" [6 1]}
+                         {"player" 56789, "move" [1 1]}
+                         {"player" 98765, "move" [1 4]}
+                         {"player" 56789, "move" [4 2]}
+                         {"player" 98765, "move" [2 3]}
+                         {"player" 56789, "move" [3 8]}
+                         {"player" 98765, "move" [8 6]}
+                         {"player" 56789, "move" [6 0]}
+                         {"player" 98765, "move" [0 2]}
+                         {"player" 56789, "move" [2 0]}
+                         {"player" 98765, "move" [0 0]}
+                         {"player" 56789, "move" [0 5]}
+                         {"player" 98765, "move" [5 6]}
+                         {"player" 56789, "move" [6 7]}
+                         {"player" 98765, "move" [7 3]}
+                         {"player" 56789, "move" [3 3]}
+                         {"player" 98765, "move" [3 4]}
+                         {"player" 56789, "move" [4 7]}
+                         {"player" 98765, "move" [7 1]}
+                         {"player" 56789, "move" [7 4]}
+                         {"player" 98765, "move" [4 6]}
+                         {"player" 56789, "move" [6 2]}
+                         {"player" 98765, "move" [4 5]}
+                         {"player" 56789, "move" [5 1]}
+                         {"player" 98765, "move" [8 3]}
+                         {"player" 56789, "move" [3 2]}
+                         {"player" 98765, "move" [8 4]}
+                         {"player" 56789, "move" [4 8]}
+                         {"player" 98765, "move" [3 7]}
+                         {"player" 56789, "move" [7 5]}
+                         {"player" 98765, "move" [6 3]}
+                         {"player" 56789, "move" [3 0]}
+                         {"player" 98765, "move" [0 4]}
+                         {"player" 56789, "move" [4 3]}
+                         {"player" 98765, "move" [3 5]}
+                         {"player" 56789, "move" [7 2]}
+                         {"player" 98765, "move" [3 6]}
+                         {"player" 56789, "move" [6 8]}
+                         {"player" 98765, "move" [0 6]}
+                         {"player" 56789, "move" [3 1]}
+                         {"player" 98765, "move" [4 0]}],
+                        "marker" {56789 "x", 98765 "o"}}]
+        (is (game/game-over? g tied-state))
+        (is (nil? (game/winner g tied-state))))
+      )))
