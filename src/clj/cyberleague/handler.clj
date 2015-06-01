8a76c026a1b77e405d8900309ed11b06abad6054
James N. V. Cash <james.nvc@gmail.com>
2015-06-01 13:41:50 -0400

    remove TODO

--- src/clj/cyberleague/handler.clj
+++ src/clj/cyberleague/handler.clj
@@ -197,7 +197,6 @@
     (PUT "/bots/:bot-id/language/:language" [bot-id language]
       (let [bot (db/with-conn (db/get-bot (to-long bot-id)))]
         (if (= id (:db/id (:bot/user bot)))
-          ; TODO: need to get different samples for different games
           (let [game (get-in bot [:bot/game :game/name])
                 code (slurp (io/resource (str "code/" game "." (case language
                                                                   "javascript" "js"
