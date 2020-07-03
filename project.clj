80f40df4214cdea7f1d84c97a055d76362451b68
Rafal Dittwald <rafal.dittwald@gmail.com>
2020-07-03 12:42:37 -0400

    Switch to datomic-free

--- project.clj
+++ project.clj
@@ -4,15 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.datomic/datomic-pro "0.9.4899"]
+                 [com.datomic/datomic-free "0.9.4899"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [me.raynes/fs "1.4.4"]
                  [org.clojure/clojurescript "1.7.145"]
                  [org.clojure/google-closure-library "0.0-20151016-61277aea"]]
 
-  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
-                                   :username "james@leanpixel.com"
-                                   :password "eb6ce9b2-92f7-4164-86c3-49e60732bba9"}}
   :uberjar-name "player-of-games.jar"
   :main cyberleague.coordinator.core
 
