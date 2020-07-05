a4f46ff91e09b9b1d961ffdc6957dc612cba99c6
Rafal Dittwald <rafal.dittwald@gmail.com>
2020-07-05 08:05:46 -0400

    Factor-out eval-move

--- src/cyberleague/coordinator/game_runner.clj
+++ src/cyberleague/coordinator/game_runner.clj
@@ -1,87 +1,66 @@
 (ns cyberleague.coordinator.game-runner
   (:require
-    [cyberleague.games.games :as games]))
+   [cyberleague.games.games :as games]))
+
+(defn eval-move [bot state]
+  ;; currently, just implemented for cljs
+  (let [bot-function (eval (read-string (:bot/deployed-code bot)))
+        parse-move (fn [m] (if (string? m) (read-string m) m))]
+    (->> state
+         bot-function
+         parse-move)))
+
+(defn run-move [bot state game-engine]
+  (let [move (try
+               (eval-move bot (games/anonymize-state-for game-engine (:db/id bot) state))
+               (catch Exception e
+                 (throw (ex-info "GameError"
+                                 {:error :exception-executing
+                                  :info (str e)
+                                  :bot (:db/id bot)
+                                  :game-state state}))))]
+    (cond
+      (not (games/valid-move? game-engine move))
+      (throw (ex-info "GameError"
+                      {:error :invalid-move
+                       :move {:bot (:db/id bot)
+                              :move move}
+                       :game-state state}))
+
+      (not (games/legal-move? game-engine state (:db/id bot) move))
+      (throw (ex-info "GameError"
+                      {:error :illegal-move
+                       :move {:bot (:db/id bot)
+                              :move move}
+                       :game-state state}))
+
+      :else
+      move)))
 
 (defn run-game [game bots]
-  (let [g (games/make-engine game)
-        nplayers (games/number-of-players g)
-        simultaneous? (games/simultaneous-turns? g)
-        parse-move (fn [m] (if (string? m) (read-string m) m))
-        bots (->> bots
-                  (map (fn [bot]
-                         {:db/id (:db/id bot)
-                          :bot/function (eval (read-string (:bot/deployed-code bot)))})))]
+  (let [game-engine (games/make-engine game)
+        nplayers (games/number-of-players game-engine)]
     (assert (= nplayers (count bots))
-      (str "Wrong number of players (" (count bots) ") for " (:game/name game)))
+            (str "Wrong number of players (" (count bots) ") for " (:game/name game)))
     (try
-      (loop [state (games/init-state g (map :db/id bots))
+      (loop [state (games/init-state game-engine (map :db/id bots))
              players (cycle bots)]
-        (if (games/game-over? g state)
+        (if (games/game-over? game-engine state)
           {:error false
-           :winner (games/winner g state)
+           :winner (games/winner game-engine state)
            :history (state "history")}
-          (if simultaneous?
+          (if (games/simultaneous-turns? game-engine)
             ;; For simulatenous turns, get all the moves
             (let [moves (reduce
-                          (fn [moves bot]
-                            (let [move (try
-                                         (parse-move
-                                           ((:bot/function bot)
-                                            (games/anonymize-state-for g (:db/id bot) state)))
-                                         (catch Exception e
-                                           (throw (ex-info "GameError"
-                                                    {:error :exception-executing
-                                                     :info (str e)
-                                                     :bot (:db/id bot)
-                                                     :game-state state}))))]
-                              (cond
-                                (not (games/valid-move? g move))
-                                (throw (ex-info "GameError"
-                                         {:error :invalid-move
-                                          :move {:bot (:db/id bot)
-                                                 :move move}
-                                          :game-state state}))
-
-                                (not (games/legal-move? g state (:db/id bot) move))
-                                (throw (ex-info "GameError"
-                                         {:error :illegal-move
-                                          :move {:bot (:db/id bot)
-                                                 :move move}
-                                          :game-state state}))
-
-                                :else (assoc moves (:db/id bot) move))))
-                          {}
-                          (take nplayers players))]
-              (recur (games/next-state g state moves) players))
+                         (fn [moves bot]
+                           (assoc moves (:db/id bot) (run-move bot state game-engine)))
+                         {}
+                         (take nplayers players))]
+              (recur (games/next-state game-engine state moves) players))
             ;; For one-at-a-time, just get the next player's move
             (let [bot (first players)
-                  move (try
-                         (parse-move
-                           ((:bot/function bot)
-                            (games/anonymize-state-for g (:db/id bot) state)))
-                         (catch Exception e
-                           (throw (ex-info "GameError"
-                                    {:error :exception-executing
-                                     :info (str e)
-                                     :bot (:db/id bot)
-                                     :game-state state}))))]
-              (cond
-                (not (games/valid-move? g move))
-                (throw (ex-info "GameError"
-                         {:error :invalid-move
-                          :move {:bot (:db/id bot)
-                                 :move move}
-                          :game-state state}))
-
-                (not (games/legal-move? g state (:db/id bot) move))
-                (throw (ex-info "GameError"
-                         {:error :illegal-move
-                          :move {:bot (:db/id bot)
-                                 :move move}
-                          :game-state state}))
-
-                :else (recur (games/next-state g state {(:db/id bot) move})
-                             (next players)))))))
-
+                  move (run-move bot state game-engine)]
+              (recur (games/next-state game-engine state {(:db/id bot) move})
+                     (next players))))))
       (catch Exception e
         (ex-data e)))))
