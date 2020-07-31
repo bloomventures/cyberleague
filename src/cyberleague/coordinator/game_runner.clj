560d2804bc6b5e6dd8ab03acee33ef84cc8cec3c
Rafal Dittwald <rafal.dittwald@gmail.com>
2020-07-31 17:20:42 -0400

    Fix broken game_runner (due to moving stuff between namespaces)
    Broken since 950732

--- src/cyberleague/coordinator/game_runner.clj
+++ src/cyberleague/coordinator/game_runner.clj
@@ -3,7 +3,7 @@
    [clojure.data.json :as json]
    [cyberleague.coordinator.evaluators.api :as evaluator.api]
    [cyberleague.coordinator.evaluators.clojure]
-   [cyberleague.games.games :as games]))
+   [cyberleague.games.protocol :as game-engine.protocol]))
 
 (defn eval-move
   [bot state]
@@ -15,7 +15,7 @@
 
 (defn run-move [bot state game-engine]
   (let [move (try
-               (eval-move bot (games/anonymize-state-for game-engine (:db/id bot) state))
+               (eval-move bot (game-engine.protocol/anonymize-state-for game-engine (:db/id bot) state))
                (catch Exception e
                  (throw (ex-info "GameError"
                                  {:error :exception-executing
@@ -23,14 +23,14 @@
                                   :bot (:db/id bot)
                                   :game-state state}))))]
     (cond
-      (not (games/valid-move? game-engine move))
+      (not (game-engine.protocol/valid-move? game-engine move))
       (throw (ex-info "GameError"
                       {:error :invalid-move
                        :move {:bot (:db/id bot)
                               :move move}
                        :game-state state}))
 
-      (not (games/legal-move? game-engine state (:db/id bot) move))
+      (not (game-engine.protocol/legal-move? game-engine state (:db/id bot) move))
       (throw (ex-info "GameError"
                       {:error :illegal-move
                        :move {:bot (:db/id bot)
@@ -41,29 +41,29 @@
       move)))
 
 (defn run-game [game bots]
-  (let [game-engine (games/make-engine game)
-        nplayers (games/number-of-players game-engine)]
+  (let [game-engine (game-engine.protocol/make-engine game)
+        nplayers (game-engine.protocol/number-of-players game-engine)]
     (assert (= nplayers (count bots))
             (str "Wrong number of players (" (count bots) ") for " (:game/name game)))
     (try
-      (loop [state (games/init-state game-engine (map :db/id bots))
+      (loop [state (game-engine.protocol/init-state game-engine (map :db/id bots))
              players (cycle bots)]
-        (if (games/game-over? game-engine state)
+        (if (game-engine.protocol/game-over? game-engine state)
           {:error false
-           :winner (games/winner game-engine state)
+           :winner (game-engine.protocol/winner game-engine state)
            :history (state "history")}
-          (if (games/simultaneous-turns? game-engine)
+          (if (game-engine.protocol/simultaneous-turns? game-engine)
             ;; For simulatenous turns, get all the moves
             (let [moves (->> (take nplayers players)
                              (map (fn [bot]
                                     [(:db/id bot) (run-move bot state game-engine)]))
                              (into {}))]
-              (recur (games/next-state game-engine state moves)
+              (recur (game-engine.protocol/next-state game-engine state moves)
                      players))
             ;; For one-at-a-time, just get the next player's move
             (let [bot (first players)
                   move (run-move bot state game-engine)]
-              (recur (games/next-state game-engine state {(:db/id bot) move})
+              (recur (game-engine.protocol/next-state game-engine state {(:db/id bot) move})
                      (next players))))))
       (catch Exception e
         (ex-data e)))))
