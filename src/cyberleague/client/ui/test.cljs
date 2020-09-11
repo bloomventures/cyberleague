8782025d88231e32b3fc785984cae3b1c2f572c5
Rafal Dittwald <rafal.dittwald@gmail.com>
2020-09-11 16:43:21 -0400

    Minor tweak

--- src/cyberleague/client/ui/test.cljs
+++ src/cyberleague/client/ui/test.cljs
@@ -4,20 +4,20 @@
 
 (defn test-view
   [match bot]
-  [:div.test
-   (when match
-     [:p
-      (cond
-        (:match/info match)
-        (:match/info match)
+  (when match
+    [:div.test
+     ;; match/info contains error information if there is an error
+     (if (:match/info match)
+       [:p (:match/info match)]
+       [:<>
+        [:p
+         (cond
+           (nil? (:winner match))
+           "Tie!"
 
-        (nil? (:winner match))
-        "Tie!"
+           (= (:bot/id bot) (:winner match))
+           "You Won!"
 
-        (= (:bot/id bot) (:winner match))
-        "You Won!"
-
-        :else
-        "You Lost!")])
-   (when (and match (nil? (:match/info match)))
-     [match-results-view match])])
+           :else
+           "You Lost!")]
+        [match-results-view match]])]))
