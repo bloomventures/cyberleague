e292dc390ef78f5017d7f4eb7e87f3b3a043743b
Christopher Spraggett <cspraggett@gmail.com>
2020-09-15 11:39:18 -0400

    Highlight winner on match-card

--- src/cyberleague/client/ui/match_card.cljs
+++ src/cyberleague/client/ui/match_card.cljs
@@ -12,9 +12,14 @@
       [:a.close {:on-click (fn [_] (state/close-card! card))} "×"]]
      [:div.content
       (let [[bot1 bot2] (:match/bots match)]
+        (println "match" (:match/winner match) (:bot/id bot2))
         [:h1
-         [:a {:on-click (fn [_] (state/nav! :card.type/bot (:bot/id bot1)))} (:bot/name bot1)]
+         [:a {:style (when (= (:match/winner match) (:bot/id bot1)) {:fontWeight "bolder"
+                                                                     :color "red"})
+              :on-click (fn [_] (state/nav! :card.type/bot (:bot/id bot1)))} (:bot/name bot1)]
          " vs "
-         [:a {:on-click (fn [_] (state/nav! :card.type/bot (:bot/id bot2)))} (:bot/name bot2)]])
+         [:a {:style (when (= (:match/winner match) (:bot/id bot2)) {:fontWeight "bolder"
+                                                                     :color "red"})
+              :on-click (fn [_] (state/nav! :card.type/bot (:bot/id bot2)))} (:bot/name bot2)]])
 
       [match-results-view match]]]))
