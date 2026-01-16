892ec1641413e8d3791c62a8cf603c1c8a6d795b
Epidiah Ravachol <epidiah@gmail.com>
2026-01-16 11:59:52 -0500

    WIP

--- src/cyberleague/coordinator/core.clj
+++ src/cyberleague/coordinator/core.clj
@@ -28,33 +28,35 @@
                    (-> (into {} player-2) (assoc :db/id (:db/id player-2)
                                                  :bot/code (db/deployed-code (:db/id player-2))))]))]
     #_(println (str (:db/id player-1) " vs " (:db/id player-2) ": " (:game.result/winner result)))
-    (if (false? (:game.result/error result))
-      ; TODO: handle ties?
-      (db/with-conn
-        (db/create-entity! (merge
-                            {:match/bots [(:db/id player-1) (:db/id player-2)]
-                             :match/state-history (pr-str (result :game.result/state-history))
-                             :match/moves (pr-str (result :game.result/history))}
-                            ;; optionally add this in b/c datomic does not allow nils
-                            (when-let [winner (:game.result/winner result)]
-                              {:match/winner winner})))
-        (ranking/update-rankings! player-1 player-2 (:game.result/winner result)))
-      (if (= (:error result) :exception-executing)
-        (let [errd-bot (if (= (:db/id player-1) (:bot result))
+    (if-let [{:move.error/keys [data type] :as error} (:game.result/error result)]
+      (case type
+        :move.error.type/invalid-code
+        (let [errd-bot (if (= (:db/id player-1) (:bot-id data))
                          player-1 player-2)]
-          (println "Exception executing, will disable:" (:db/id errd-bot) (:info result))
+          (println "Exception executing, will disable:" (:db/id errd-bot) (:message data))
           (db/disable-bot! errd-bot))
-        (let [[winner cheater] (if (= (get-in result [:move :bot]) (:db/id player-1))
+
+        (:move.error.type/invalid-move :move.error.type/illegal-move)
+        (let [[winner cheater] (if (= (:bot-id data) (:db/id player-1))
                                  [player-2 player-1]
                                  [player-1 player-2])]
           (println "Bad move from " cheater)
           (db/with-conn
             (db/create-entity! {:match/bots [(:db/id player-1) (:db/id player-2)]
                                :match/error true
-                               :match/moves (pr-str (conj (get-in result [:game-state :history])
-                                                          (get-in result [:move :move])))
+                               :match/moves (pr-str (:game.result/history result))
                                :match/winner (:db/id winner)})
-            (db/disable-cheater! cheater)))))))
+            (db/disable-cheater! cheater))))
+      ; TODO: handle ties?
+      (db/with-conn
+        (db/create-entity! (merge
+                            {:match/bots [(:db/id player-1) (:db/id player-2)]
+                             :match/state-history (pr-str (:game.result/state-history result))
+                             :match/moves (pr-str (:game.result/history result))}
+                            ;; optionally add this in b/c datomic does not allow nils
+                            (when-let [winner (:game.result/winner result)]
+                              {:match/winner winner})))
+        (ranking/update-rankings! player-1 player-2 (:game.result/winner result))))))
 
 #_(let [[game bots] (first (db/with-conn (db/active-bots)))]
     (->> bots
