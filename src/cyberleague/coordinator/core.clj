f6e5dbe6daca4c5a80f0849b970cc1a1ccddff1c
Rafal Dittwald <rafal.dittwald@gmail.com>
2026-01-30 12:07:13 -0500

    Fix coordinator exception when not enough bots for a game
    rand-nth was exploding when receiving an empty list (for player-1 or player-2)
    
    Co-Authored-By: Epidiah Ravachol <epidiah@gmail.com>

--- src/cyberleague/coordinator/core.clj
+++ src/cyberleague/coordinator/core.clj
@@ -1,6 +1,7 @@
 (ns cyberleague.coordinator.core
   (:gen-class)
   (:require
+   [hyperfiddle.rcf :as rcf]
    [cyberleague.db.core :as db]
    [cyberleague.config :refer [config]]
    [cyberleague.coordinator.game-runner :as game-runner]
@@ -8,55 +9,72 @@
 
 (defonce run? (atom false))
 
-(defn run-one-game! [game all-bots]
-  (let [player-1 (rand-nth all-bots)
-        player-2 (->> all-bots
-                      (remove (partial = player-1))
-                      (sort-by (fn [bot] (Math/abs
-                                          (- (:bot/rating bot)
-                                             (:bot/rating player-1)))))
-                      (take 10)
-                      (sort-by :bot/rating-dev #(compare %2 %1))
-                      (take 5)
-                      rand-nth)
-        ;_ (print "Starting " (:db/id player-1) " vs " (:db/id player-2) "...")
-        result (game-runner/run-game
-                (into {} game)
-                (db/with-conn
-                  [(-> (into {} player-1) (assoc :db/id (:db/id player-1)
-                                                 :bot/code (db/deployed-code (:db/id player-1))))
-                   (-> (into {} player-2) (assoc :db/id (:db/id player-2)
-                                                 :bot/code (db/deployed-code (:db/id player-2))))]))]
-    #_(println (str (:db/id player-1) " vs " (:db/id player-2) ": " (:game.result/winner result)))
-    (if-let [{:move.error/keys [data type] :as error} (:game.result/error result)]
-      (case type
-        :move.error.type/invalid-code
-        (let [errd-bot (if (= (:db/id player-1) (:bot-id data))
-                         player-1 player-2)]
-          (println "Exception executing, will disable:" (:db/id errd-bot) (:message data))
-          (db/disable-bot! errd-bot))
+(defn select-players
+  [_game active-bots]
+  (when (seq active-bots) ;; possible to have no active bots
+    (let [player-1 (rand-nth active-bots)
+          player-2-candidates (->> active-bots
+                                   (remove (partial = player-1))
+                                   (sort-by (fn [bot] (Math/abs
+                                                       (- (:bot/rating bot)
+                                                          (:bot/rating player-1)))))
+                                   (take 10)
+                                   (sort-by :bot/rating-dev #(compare %2 %1))
+                                   (take 5))]
+      (when (seq player-2-candidates) ;; possible to have no other candidates
+        (let [player-2 (rand-nth player-2-candidates)]
+          {:player-1 player-1
+           :player-2 player-2})))))
 
-        (:move.error.type/invalid-move :move.error.type/illegal-move)
-        (let [[winner cheater] (if (= (:bot-id data) (:db/id player-1))
-                                 [player-2 player-1]
-                                 [player-1 player-2])]
-          (println "Bad move from " cheater)
-          (db/with-conn
-            (db/create-entity! {:match/bots [(:db/id player-1) (:db/id player-2)]
-                               :match/error true
-                               :match/moves (pr-str (:game.result/history result))
-                               :match/winner (:db/id winner)})
-            (db/disable-cheater! cheater))))
-      ; TODO: handle ties?
-      (db/with-conn
-        (db/create-entity! (merge
-                            {:match/bots [(:db/id player-1) (:db/id player-2)]
-                             :match/state-history (pr-str (:game.result/state-history result))
-                             :match/moves (pr-str (:game.result/history result))}
-                            ;; optionally add this in b/c datomic does not allow nils
-                            (when-let [winner (:game.result/winner result)]
-                              {:match/winner winner})))
-        (ranking/update-rankings! player-1 player-2 (:game.result/winner result))))))
+#_(apply select-players (first (db/with-conn (db/active-bots))))
+
+(rcf/tests
+ (select-players nil []) := nil
+ (select-players nil [{:bot/rating 100}]) := nil
+ (select-players nil [{:bot/rating 100} {:bot/rating 200}]) := {:player-1 _ :player-2 _})
+
+(defn run-one-game! [game active-bots]
+  (when-let [{:keys [player-1 player-2]} (select-players game active-bots)]
+    #_(print "Starting " (:db/id player-1) " vs " (:db/id player-2) "...")
+    (let [result (game-runner/run-game
+                  (into {} game)
+                  (db/with-conn
+                   [(-> (into {} player-1) (assoc :db/id (:db/id player-1)
+                                                  :bot/code (db/deployed-code (:db/id player-1))))
+                    (-> (into {} player-2) (assoc :db/id (:db/id player-2)
+                                                  :bot/code (db/deployed-code (:db/id player-2))))]))]
+      #_(println (str (:db/id player-1) " vs " (:db/id player-2) ": " (:game.result/winner result)))
+      (if-let [{:move.error/keys [data type] :as error} (:game.result/error result)]
+        (case type
+          :move.error.type/invalid-code
+          (let [errd-bot (if (= (:db/id player-1) (:bot-id data))
+                           player-1
+                           player-2)]
+            (println "Exception executing, will disable:" (:db/id errd-bot) (:message data))
+            (db/disable-bot! errd-bot))
+
+          (:move.error.type/invalid-move :move.error.type/illegal-move)
+          (let [[winner cheater] (if (= (:bot-id data) (:db/id player-1))
+                                   [player-2 player-1]
+                                   [player-1 player-2])]
+            (println "Bad move from " cheater)
+            (db/with-conn
+             (db/create-entity! {:match/bots [(:db/id player-1) (:db/id player-2)]
+                                 :match/error true
+                                 :match/state-history (pr-str (:game.result/state-history result))
+                                 :match/moves (pr-str (:game.result/history result))
+                                 :match/winner (:db/id winner)})
+             (db/disable-cheater! cheater))))
+        ; TODO: handle ties?
+        (db/with-conn
+         (db/create-entity! (merge
+                             {:match/bots [(:db/id player-1) (:db/id player-2)]
+                              :match/state-history (pr-str (:game.result/state-history result))
+                              :match/moves (pr-str (:game.result/history result))}
+                             ;; optionally add this in b/c datomic does not allow nils
+                             (when-let [winner (:game.result/winner result)]
+                               {:match/winner winner})))
+         (ranking/update-rankings! player-1 player-2 (:game.result/winner result)))))))
 
 #_(let [[game bots] (first (db/with-conn (db/active-bots)))]
     (->> bots
@@ -66,12 +84,14 @@
                  :bot/code (db/with-conn (db/deployed-code (:db/id bot)))}))
          (game-runner/run-game game)))
 
+
+
 (defn run-games! []
   (println "Running games")
   (reset! run? true)
   (while @run?
-    (doseq [[game all-bots] (db/with-conn (db/active-bots))]
-      (run-one-game! game all-bots)
+    (doseq [[game active-bots] (db/with-conn (db/active-bots))]
+      (run-one-game! game active-bots)
       (Thread/sleep (config :coordinator-delay)))))
 
 (defn start! []
