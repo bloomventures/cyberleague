85b3f5adf4a77e0261654df61f7b9bda408b31df
Rafal Dittwald <rafal.dittwald@gmail.com>
2026-02-19 09:40:14 -0500

    WIP

--- project.clj
+++ project.clj
@@ -8,41 +8,43 @@
 
   :test-paths ["test"]
 
-  :dependencies [[com.hyperfiddle/rcf "20220926-202227"]]
-  :profiles {:server {:dependencies [[org.clojure/clojure "1.12.4"] ; Omni currently broken without this
-                                     [io.bloomventures/omni "0.34.1"]
-                                     [com.datomic/peer "1.0.6733"
+  :dependencies [[com.hyperfiddle/rcf "20220926-202227"
+                  :exclusions [org.clojure/clojure]]]
+  :profiles {:server {:dependencies [; [org.clojure/clojure "1.12.4"] ;; from omni
+                                     [io.bloomventures/omni "0.35.0"]
+                                     [com.datomic/peer "1.0.7491"
                                       :exclusions
-                                      [com.google.guava/guava
-                                       commons-codec
-                                       org.clojure/tools.cli
-                                       org.slf4j/slf4j-nop
-                                       org.slf4j/jcl-over-slf4j
-                                       org.slf4j/slf4j-api]]
+                                      [org.apache.httpcomponents/httpclient
+                                       joda-time
+                                       org.clojure/clojure
+                                       commons-io]]
 
                                      [com.taoensso/tempel "1.1.0"]
-                                     [tada "0.3.0"]
+                                     [tada "0.3.0"
+                                      :exclusions [org.clojure/clojure
+                                                   metosin/malli
+                                                   org.clojure/spec.alpha]]
 
                                      ;; client
-                                     [markdown-clj "1.10.4"]
-                                     #_[reagent "0.10.0"]
-                                     #_[org.clojure/clojurescript "1.10.764"]
+                                     [markdown-clj "1.10.4"
+                                      :exclusions [org.clojure/clojure]]
+                                     #_[reagent "0.10.0"] ;; from omni
+                                     #_[org.clojure/clojurescript "1.10.764"] ;; from omni
                                      [cljsjs/codemirror "5.44.0-1"]
                                      [cljsjs/d3 "3.5.5-2"]
-                                     [zprint "1.2.9"]
+                                     #_[zprint "1.2.9"] ;; from omni->commons
 
                                      ;; coordinator
-                                     [org.clojure/math.numeric-tower "0.0.4"]
+                                     [org.clojure/math.numeric-tower "0.0.4"
+                                      :exclusions [org.clojure/clojure]]
                                      #_[org.clojure/data.json "1.0.0"]
-                                     [org.babashka/sci "0.12.51"
-                                      :exclusions [org.clojure/tools.reader]]
+                                     #_[org.babashka/sci "0.12.51"] ;; from omni->commons
                                      ;; if need macos or windows, see:
                                      ;; https://search.maven.org/search?q=g:com.eclipsesource.j2v8
                                      [com.eclipsesource.j2v8/j2v8_linux_x86_64 "4.8.0"]
 
                                      ;; registrar
-                                     ;; using malli from tada
-                                     ;; [metosin/malli "0.2.1"]
+                                     #_[metosin/malli "0.2.1"] ;; from omni->commoms
                                      ]
 
                       :main cyberleague.core
@@ -54,10 +56,11 @@
                                       [http-kit "2.8.0"]
                                       [com.nextjournal/beholder "1.0.2"]]
                        :repl-options {:init-ns cyberleague.cli.core}}
-             :uberjar [:server
-                       {:aot :all
+             :uberjar- {:aot :all
                         :dependencies
                         [[org.postgresql/postgresql "42.2.2"]]
                         :prep-tasks
                         [["omni" "compile"]
-                         "compile"]}]})
+                         "compile"]}
+             :uberjar [:server
+                       :uberjar-]})
