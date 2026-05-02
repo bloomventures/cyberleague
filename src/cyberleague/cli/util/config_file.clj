9dbb6e37b51a6878f7362f61707a2b88bba6172c
Rafal Dittwald <rafal.dittwald@gmail.com>
2026-05-02 19:48:39 -0400

    Look for cli config file in all parent directories

--- src/cyberleague/cli/util/config_file.clj
+++ src/cyberleague/cli/util/config_file.clj
@@ -1,16 +1,29 @@
 (ns cyberleague.cli.util.config-file
   (:refer-clojure :exclude [read])
   (:require
+   [clojure.java.io :as io]
    [cyberleague.cli.util.ednf :as ednf]))
 
-(def path "cyberleague-token.edn")
+(def filename "cyberleague-token.edn")
+
+(defn- find-path
+  []
+  (->> (.getAbsoluteFile (io/file ""))
+       (iterate (fn [dir] (.getParentFile dir)))
+       (take-while some?)
+       (map (fn [dir] (io/file dir filename)))
+       (filter (fn [f] (.exists f)))
+       first))
 
 (defn read
   []
-  (ednf/read path))
+  (when-let [f (find-path)]
+    (ednf/read (.getPath f))))
 
 (defn set-kv!
   [k v]
-  (-> (read)
-      (assoc k v)
-      (->> (ednf/write! path))))
+  (let [p (or (some-> (find-path) .getPath)
+              filename)]
+    (-> (read)
+        (assoc k v)
+        (->> (ednf/write! p)))))
